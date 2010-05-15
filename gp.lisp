1e861eb893ff2419185965f150dae10d1a68a84a
Martin Brandenburg <brandenburgmk@gmail.com>
2010-05-15 01:37:42 -0400

    I pretty much rewrote the whole mutate function. Also, randomfun has been added. It generates random arithmetic functions.

--- gp.lisp
+++ gp.lisp
@@ -10,17 +10,30 @@
    (lambda (f)
      (funcall r (lambda (x) (funcall (funcall f f) x))))))
 
-; So apparently labels is better than Y, but I'm keeping the Y.
 (defun mutate (form)
   "Mutate takes a form and modifies the numbers randomly."
   (let ((sign (let ((x (random 2)))
                 (if (= x 0) #'+ #'-))))
-    (mapcar (Y (lambda (fun)
-                 (lambda (n)
-                   (cond
-                     ((eq (class-of n) (find-class 'fixnum))
-                      (funcall sign (random 5) n))
-                     ((eq (class-of n) (find-class 'cons))
-                      (mapcar fun n))
-                     (t n)))))
-             form)))
+    (labels ((mutate-num (n)
+               (cond
+                 ((eq (class-of n) (find-class 'fixnum))
+                  (funcall sign
+                           (let ((x (random 5)))
+                             (if (and (eq (first form) '/) (= x 0)) 1 x))
+                           n))
+                 ((eq (class-of n) (find-class 'cons))
+                  (mapcar #'mutate-num n))
+                 (t n))))
+      (mapcar #'mutate-num form))))
+
+(defun randomfun ()
+  (let ((arithmetic (let ((x (random 4)))
+                      (cond
+                        ((= x 0) '+)
+                        ((= x 1) '-)
+                        ((= x 2) '*)
+                        ((= x 3) '/)))))
+    (let ((n1 (random 5))
+          (n2 (let ((x (random 5)))
+                (if (and (eq arithmetic '/) (= x 0)) 1 x))))
+      (list arithmetic n1 n2))))
