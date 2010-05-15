c4b19cd555d2c51715267e6a20a2a0c3eb0a7980
Martin Brandenburg <brandenburgmk@gmail.com>
2010-05-15 02:29:38 -0400

    Remove the Y Combinator, Refactor random-number-with-output, and Make it impervious to division by zero (I hope!).

--- gp.lisp
+++ gp.lisp
@@ -4,23 +4,21 @@
 
 (in-package :gp.goof)
 
-; Look! A Y Combinator.
-(defun Y (r)
-  ((lambda (f) (funcall f f))
-   (lambda (f)
-     (funcall r (lambda (x) (funcall (funcall f f) x))))))
+(defun random-number-with-offset (sign offset)
+  (let ((x (random 5)))
+    (if (= (funcall (symbol-function sign) x offset) 0)
+        (random-number-with-offset sign offset)
+        x)))
 
 (defun mutate (form)
   "Mutate takes a form and modifies the numbers randomly."
   (let ((sign (let ((x (random 2)))
-                (if (= x 0) #'+ #'-))))
+                (if (= x 0) '+ '-))))
     (labels ((mutate-num (n)
                (cond
                  ((eq (class-of n) (find-class 'fixnum))
-                  (funcall sign
-                           (let ((x (random 5)))
-                             (if (and (eq (first form) '/) (= x 0)) 1 x))
-                           n))
+                  (funcall (symbol-function sign)
+                           (random-number-with-offset sign n) n))
                  ((eq (class-of n) (find-class 'cons))
                   (mapcar #'mutate-num n))
                  (t n))))
@@ -34,6 +32,5 @@
                         ((= x 2) '*)
                         ((= x 3) '/)))))
     (let ((n1 (random 5))
-          (n2 (let ((x (random 5)))
-                (if (and (eq arithmetic '/) (= x 0)) 1 x))))
+          (n2 (+ (random 4) 1)))
       (list arithmetic n1 n2))))
